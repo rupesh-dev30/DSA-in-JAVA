1e2af9619176b5e65f2333c4bb3c388caffa4e64
rupesh-dev30 <cgcrupesh@gmail.com>
2024-05-01 00:34:33 +0530

    inverted triangle

---
+++ Pattern/invertedTriangle.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class invertedTriangle {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int number = sc.nextInt();
+    sc.close();
+
+    for(int i=0 ; i<number ; i++){
+      for(int j=0 ; j<number-i ; j++){
+        System.out.print("* ");
+      }
+      System.out.println();
+    }
+  }
+}
