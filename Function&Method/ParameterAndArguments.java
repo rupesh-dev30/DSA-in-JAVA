2cf4e170645fb24c668ae72b48535ab9b67826fa
rupesh-dev30 <cgcrupesh@gmail.com>
2024-05-01 01:21:05 +0530

    Parameter And Arguments

---
+++ Function&Method/ParameterAndArguments.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class ParameterAndArguments {
+  public static int add(int a, int b) {  //parameter or formal parameter
+    return a + b;
+  }
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    sc.close();
+    int result = add(a, b);      //Arguments or actual parameter
+    System.out.println("The sum of " + a + " and " + b + " is " + result);
+  }
+}
