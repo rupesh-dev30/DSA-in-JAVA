5ff8d5c1edb8846b4340ce1a17d28643a35ba70d
rupesh-dev30 <cgcrupesh@gmail.com>
2024-05-01 00:43:13 +0530

    Character Trianlge

---
+++ Pattern/PrintCharacterPattern.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class PrintCharacterPattern {
+  public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    char ch = 'A';
+    for(int i=0; i<n; i++){
+      for(int j=0; j<i+1; j++){
+        System.out.print(ch+" ");
+        ch++;
+      }
+      System.out.println();
+    }
+  }
+}
