980d2e3e525b7d412c4ce96a15f8e0faa0a44af3
rupesh-dev30 <cgcrupesh@gmail.com>
2024-04-30 20:21:15 +0530

    sumOfNaturalNumber

--- Loops/While.java
+++ Loops/While.java
@@ -1,7 +1,11 @@
+import java.util.Scanner;
+
 public class While {
   public static void main(String[] args) {
-    int i = 0 ;
-    while(i<10){
+    Scanner sc = new Scanner(System.in);
+    int number = sc.nextInt();
+    int i = 1;
+    while(i<=number){
       System.out.println(i);
       i++;
     }
---
+++ Loops/sumOfFirstNNaturalNumber.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class sumOfFirstNNaturalNumber {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int num = sc.nextInt();
+    int i = 1;
+    int sum = 0;
+    while(i<=num){
+      sum += i;
+      i++;
+    }
+
+    System.out.println("sum is "+sum);
+  }
+}
