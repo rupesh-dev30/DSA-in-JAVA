05e3929f42fb71db7d400f0f5f4ed4e32b649b7f
rupesh-dev30 <cgcrupesh@gmail.com>
2024-04-30 15:24:35 +0530

    RelationalOperatot

---
+++ Operators/RelationalOperator.java
@@ -0,0 +1,13 @@
+public class RelationalOperator {
+  public static void main(String Args[]){
+    int a = 5;
+    int b = 6;
+
+    if(a==b){
+      System.out.println("a is not equal to b");
+    }
+    else{
+      System.out.println("a is not equal to b");
+    }
+  }
+}
