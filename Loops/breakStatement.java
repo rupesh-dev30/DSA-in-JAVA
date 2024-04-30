fc53e10843d2d8f2ecb9d2c485cc24fb3101e3b5
rupesh-dev30 <cgcrupesh@gmail.com>
2024-04-30 20:45:59 +0530

    break Statement

---
+++ Loops/breakStatement.java
@@ -0,0 +1,12 @@
+public class breakStatement {
+  public static void main(String args[]){
+    int i = 1; 
+    while(i<10){
+      if(i==5){
+        break;
+      }
+      System.out.println(i);
+      i++;
+    }
+  }
+}
