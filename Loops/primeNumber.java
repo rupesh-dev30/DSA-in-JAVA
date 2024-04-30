c10f1c7642480aafe7c4fd6d73f92c9e77b45bcd
rupesh-dev30 <cgcrupesh@gmail.com>
2024-05-01 00:24:46 +0530

    Prime Number Optimize

--- Loops/primeNumber.java
+++ Loops/primeNumber.java
@@ -1,17 +1,18 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class primeNumber {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int number = sc.nextInt();
+    sc.close();
 
     if(number == 1){
       System.out.println("Not Prime nor Composite");
       return;
     }
-    
+
     int i = 2;
-    while(i<number){
+    while(i<=Math.sqrt(number)){
       
       if(number%i == 0){
         System.out.println("Composite Number");
