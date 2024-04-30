1f37a771b818bb10868e43118f0e45b9a1c59062
rupesh-dev30 <cgcrupesh@gmail.com>
2024-04-30 14:23:50 +0530

    TypeConversion

---
+++ Variable&DataVairables/TypeConversion.java
@@ -0,0 +1,23 @@
+public class TypeConversion {
+  public static void main(String[] args) {
+    int a = 25;
+    long b = a;
+    System.out.println(b);
+  }
+}
+
+
+
+// small size to large size is possible but large size to small size is not possible
+
+// int to long is possible but long to int is not possible
+// int to float is possible but float to int is not possible
+
+// int to double is possible but double to int is not possible
+// int to char is possible but char to int is not possible
+
+// int to boolean is possible but boolean to int is not possible
+// int to String is possible but String to int is not possible
+
+// int to byte is possible but byte to int is not possible
+// int to short is possible but short to int is not possible
