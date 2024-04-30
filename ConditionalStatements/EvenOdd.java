f955cbc58e88e28177a954d27c5336441b977f26
rupesh-dev30 <cgcrupesh@gmail.com>
2024-04-30 18:31:15 +0530

    oddEven

---
+++ ConditionalStatements/EvenOdd.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class EvenOdd {
+  public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+
+    int num = sc.nextInt();
+
+    if(num%2 == 0){
+      System.out.println("Even Number");
+    } else{
+      System.out.println("Odd Number");
+    }
+  }
+}
