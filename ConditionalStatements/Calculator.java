4ab6c38ec3972163d290d24a2324f60f7ee1ec50
rupesh-dev30 <cgcrupesh@gmail.com>
2024-04-30 19:12:11 +0530

    Calculator

---
+++ ConditionalStatements/Calculator.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Calculator {
+  public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt() ;
+    char operator = sc.next().charAt(0);
+    int b = sc.nextInt() ;
+    sc.close();
+
+    switch (operator) {
+      case '+':
+        System.out.println(a + b);
+        break;
+      case '-':
+        System.out.println(a - b);
+        break;
+      case '*':
+        System.out.println(a * b);
+        break;
+      case '/':
+        System.out.println(a / b);
+        break;
+      case '%':
+        System.out.println(a % b);
+        break;
+      default:
+        System.out.println("Wrong");
+        break;
+    }
+  }
+}
