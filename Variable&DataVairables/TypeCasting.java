d9230d83c3380a6f01569999b3fa891cb2c23582
rupesh-dev30 <cgcrupesh@gmail.com>
2024-04-30 14:15:32 +0530

    possible lossy conversion from float to int (ERROR)

---
+++ Variable&DataVairables/TypeCasting.java
@@ -0,0 +1,10 @@
+public class TypeCasting {
+  public static void main(String Args[]){
+    float a = 25.8f;
+    int b = a;
+    System.out.println(b);
+  }
+}
+
+
+// java doesnt allow type casting but programmer do forcely to short the code
