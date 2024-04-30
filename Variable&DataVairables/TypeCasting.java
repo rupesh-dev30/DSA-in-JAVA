321988de9737e80f479a9fa227ec2b568599e909
rupesh-dev30 <cgcrupesh@gmail.com>
2024-04-30 14:19:49 +0530

    characterToIntPossible

--- Variable&DataVairables/TypeCasting.java
+++ Variable&DataVairables/TypeCasting.java
@@ -6,8 +6,14 @@ public class TypeCasting {
 
     int b = (int) a;
     System.out.println(b);
+
+    char ch = 'b';
+    int i = ch;
+    System.out.println(i);
   }
 }
 
 
 // java doesnt allow type casting but programmer do forcely to short the code
+
+// direct casting from character to int is possible in java
