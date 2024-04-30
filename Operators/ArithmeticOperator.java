5ea293a114a47ff959c86bcda76e2bd023498bed
rupesh-dev30 <cgcrupesh@gmail.com>
2024-04-30 14:52:03 +0530

    ArithmeticOperator

---
+++ Operators/ArithmeticOperator.java
@@ -0,0 +1,14 @@
+public class ArithmeticOperator {
+  public static void main(String args[]){
+    int a = 10;
+    int b = 3;
+    System.out.println("add = " + (a+b));
+    System.out.println("sub = " + (a-b));
+    System.out.println("mul = " + (a*b));
+    System.out.println("div = " + (a/b));
+    System.out.println("modulo = " + (a%b));
+  }
+}
+
+//Binary ( +, -, *, /, %)
+//Unary (++, --)
