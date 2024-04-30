fb296c4aff42177cd4dd38d8fb6fd56de92d17e9
rupesh-dev30 <cgcrupesh@gmail.com>
2024-05-01 01:10:17 +0530

    Basic Structure of Function

---
+++ Function&Method/Function.java
@@ -0,0 +1,12 @@
+public class Function {
+  public static void printHelloWorld(){
+    System.out.println("Hello World");
+  }
+
+  public static void main(String[] args) {
+    printHelloWorld();
+    printHelloWorld();
+    printHelloWorld();
+    printHelloWorld();
+  }
+}
