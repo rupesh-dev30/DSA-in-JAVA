211a030718d260cd6f88f2b6bb8d7b0250e541e3
rupesh-dev30 <cgcrupesh@gmail.com>
2024-04-30 14:32:33 +0530

    TypePromotion

---
+++ Variable&DataVairables/TypePromotionInExpression.java
@@ -0,0 +1,44 @@
+public class TypePromotionInExpression {
+  public static void main(String[] args) {
+    /* 
+    
+    char a = 'a';
+    char b = 'b';
+    System.out.println(b-a);
+
+    */
+
+    /* 
+
+    char a = 'a';
+    char b = 'b';
+    char c = a - b;
+    System.out.println(c);
+
+    // not possible because a-b is integer and c in char. 
+
+    */
+
+    /* 
+
+    short a = 5;
+    byte b = 25;
+    char c = 'c';
+    byte bt = a+b+c;
+    System.out.println(bt);
+
+    //not possible because a+b+c is integer and bt is byte
+
+    */
+
+    short a = 5;
+    byte b = 25;
+    char c = 'c';
+    byte bt = (byte) (a+b+c);
+    System.out.println(bt);
+
+  }
+}
+
+//byte , char , short convert it to int and then execute
+//Type conversion only for experession not for variable
