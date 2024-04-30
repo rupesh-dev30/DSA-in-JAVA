c73e7747dc4f5688780d846319d9190e6d8a906e
rupesh-dev30 <cgcrupesh@gmail.com>
2024-04-30 14:43:00 +0530

    Variables

---
+++ Variable&DataVairables/Variables.java
@@ -0,0 +1,16 @@
+public class Variables {
+  public static void main(String arg[]){
+    int a = 10;
+    int b = 20;
+    System.out.println(a);
+
+    a = 50;
+    System.out.println(a);
+  }
+}
+
+
+/*
+  variable can be change further, example a while literal not
+  Literal is constant , example 1,2,3,4,5,6 are integer literal
+*/
