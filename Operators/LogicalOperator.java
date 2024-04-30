388443757449c1abc086f03a176760224d12063a
rupesh-dev30 <cgcrupesh@gmail.com>
2024-04-30 15:27:21 +0530

    LogicalOperatot

---
+++ Operators/LogicalOperator.java
@@ -0,0 +1,11 @@
+public class LogicalOperator {
+  public static void main(String[] args) {
+    // && : and
+    // || : or
+    // ! : not
+
+    System.out.println((5>8) && (5<8));
+    System.out.println((5>8) || (5<8));
+    System.out.println(!(5>8));
+  }
+}
