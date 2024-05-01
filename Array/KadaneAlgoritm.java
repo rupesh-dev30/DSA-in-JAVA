0b80a1fae92e86f0a824f455f9037fb643d1656e
rupesh-dev30 <cgcrupesh@gmail.com>
2024-05-02 00:57:06 +0530

    Kadane Algorithm

---
+++ Array/KadaneAlgoritm.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class KadaneAlgoritm {
+  public static int kadaneAlgorithm(int arr[]){
+    int maxSum = Integer.MIN_VALUE;
+    int currentSum = 0;
+    for(int i=0 ; i<arr.length ; i++){
+      currentSum += arr[i];
+      if(currentSum < 0){
+        currentSum = 0;
+      }
+      maxSum = Math.max(currentSum,maxSum);
+    }
+    return maxSum;
+  }
+  public static void main(String[] args) {
+    int arr[] = {-2,-3,4,-1,-2,1,5,-3};
+    int result = kadaneAlgorithm(arr);
+    System.out.println(result);
+  }
+}
