3fd1080b325eb3416a0f3238f0ce4159b51af86e
rupesh-dev30 <cgcrupesh@gmail.com>
2024-05-01 16:02:19 +0530

    printing all prime number

---
+++ Function&Method/PrintAllPrimeInRange.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class PrintAllPrimeInRange {
+  public static boolean isPrime(int num) {
+    int i = 2;
+    while(i <= Math.sqrt(num)){
+      if(num%i == 0){
+        return false;
+      }
+      i++;
+    }
+    return true;
+  }
+
+  public static void primeNumber(int num) {
+    for(int i = 2 ; i<=num ; i++){
+      if(isPrime(i)){
+        System.out.print(i+" ");
+      }
+    }
+  }
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int num = sc.nextInt();
+    sc.close();
+
+    primeNumber(num);
+  }
+}
