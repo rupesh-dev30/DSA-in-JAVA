477a06edc324ec2bd347a51b5098863dfe93c9f5
rupesh-dev30 <cgcrupesh@gmail.com>
2024-05-01 20:37:04 +0530

    Array as argument

---
+++ Array/ArraysAsArgument.java
@@ -0,0 +1,16 @@
+public class ArraysAsArgument {
+  public static void update(int marks[]) {
+    for(int i=0 ; i<marks.length; i++){
+      marks[i] += 1;
+    }
+  }
+
+  public static void main(String[] args) {
+
+    int marks[] = { 98, 96, 99 };
+    update(marks);
+    for(int i=0 ; i<marks.length; i++){
+      System.out.println(marks[i]);
+    }
+  }
+}
