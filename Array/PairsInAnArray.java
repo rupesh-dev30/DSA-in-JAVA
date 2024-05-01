5e5865db5b44326107c5e37c4f1c41240dccfcfd
rupesh-dev30 <cgcrupesh@gmail.com>
2024-05-02 00:05:26 +0530

    Pairs in Array

---
+++ Array/PairsInAnArray.java
@@ -0,0 +1,18 @@
+public class PairsInAnArray {
+  public static void makePair(int arr[]){
+    for(int i=0 ; i<arr.length-1 ; i++){
+      for(int j=i+1 ; j<arr.length ; j++){
+        System.out.print("( "+arr[i]+", "+arr[j]+ " ) ");
+      }
+      System.out.println();
+    }
+  }
+
+  public static void main(String[] args) {
+    int arr[] = {2,4,6,8,10};
+    //output: (2,4), (2,6) , (2,8), (2,10) ...
+    //total number of pairs = n(n+1)/2
+
+    makePair(arr);
+  }
+}
