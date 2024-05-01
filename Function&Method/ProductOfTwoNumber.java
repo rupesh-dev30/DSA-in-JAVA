00fc5c3a515b3ce3fabcf51fb7ad9f89c076bc17
rupesh-dev30 <cgcrupesh@gmail.com>
2024-05-01 15:16:47 +0530

    product of 2 number

---
+++ Function&Method/ProductOfTwoNumber.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ProductOfTwoNumber {
+  public static int product(int a, int b) {
+    return a * b;
+  }
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    sc.close();
+
+    int result = product(a, b);
+    System.out.println(result);
+  }
+}
