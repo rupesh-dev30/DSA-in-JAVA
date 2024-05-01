fd099cb67e9ff7e5cd284817339e2fce2c56c591
rupesh-dev30 <cgcrupesh@gmail.com>
2024-05-01 15:23:20 +0530

    binomial Coefficient

---
+++ Function&Method/binomialCoefficient.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class binomialCoefficient {
+  public static int factorial(int num){
+    int fact = 1;
+    for(int i=2 ; i<=num ; i++){
+      fact *= i;
+    }
+    return fact;
+  }
+
+  public static int binomial(int n, int r){
+    return (factorial(n)/(factorial(r)*factorial(n-r)));
+  }
+  
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int r = sc.nextInt();
+    sc.close();
+
+    int result = binomial(n,r);
+    System.out.println(result);
+  }
+}
