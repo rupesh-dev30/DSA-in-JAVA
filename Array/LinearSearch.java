48b5ef434b2598a6858802fe8c0274aa56e91508
rupesh-dev30 <cgcrupesh@gmail.com>
2024-05-01 20:40:18 +0530

    linear Search

---
+++ Array/LinearSearch.java
@@ -0,0 +1,22 @@
+public class LinearSearch {
+  public static boolean linearSearch(int numbers[], int key){
+    for(int i=0 ; i<numbers.length ; i++){
+      if(numbers[i] == key){
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public static void main(String args[]){
+    int numbers[] = { 2,4,6,8,10,12,16,18};
+    int key = 101;
+
+    if(linearSearch(numbers, key)){
+      System.out.println("Key is present in the given array!");
+    }
+    else{
+      System.out.println("Key is not present in the given array!");
+    }
+  }
+}
