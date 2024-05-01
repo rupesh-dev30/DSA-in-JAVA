6a2dae2f3e705860906854cd275e170a1806e9ba
rupesh-dev30 <cgcrupesh@gmail.com>
2024-05-02 00:43:17 +0530

    optimize MaxSubArray

---
+++ Array/MaxSubArraySumPrefixSum.java
@@ -0,0 +1,28 @@
+public class MaxSubArraySumPrefixSum {
+  public static int prefixSum(int arr[]){
+    int max = Integer.MIN_VALUE;
+    for(int i=1 ; i<arr.length ; i++){
+      arr[i] += arr[i-1];
+    }
+
+    for(int i=0 ; i<arr.length; i++){
+      int sum = 0;
+      for(int j=i ; j<arr.length ; j++){
+        if(i == 0){
+          sum = arr[j];
+        }else{
+          sum = arr[j] - arr[i-1];
+        }
+        if(sum > max){
+          max = sum;
+        }
+      }
+    }
+    return max;
+  }
+  public static void main(String[] args) {
+    int arr[] = {1,-2,6,-1,3};
+    int result = prefixSum(arr);
+    System.out.println(result);
+  }
+}
