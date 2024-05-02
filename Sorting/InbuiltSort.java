8820ab5e4dfe4f9f7d4128c76688afd7b68330a8
rupesh-dev30 <cgcrupesh@gmail.com>
2024-05-03 01:12:52 +0530

    Reverse order (only work on Object)

--- Sorting/InbuiltSort.java
+++ Sorting/InbuiltSort.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Collections;
 
 public class InbuiltSort {
   public static void main(String[] args) {
@@ -14,6 +15,13 @@ public class InbuiltSort {
     Arrays.sort(arr1, 0 , 3);
     for(int i=0 ; i<arr1.length; i++){
       System.out.print(arr1[i]+" ");
+    }
+    System.out.println();
+
+    Integer[] arr2 = { 4,7,2,17,78,8,9,1 };
+    Arrays.sort(arr2, Collections.reverseOrder());
+    for(int i=0 ; i<arr2.length; i++){
+      System.out.print(arr2[i]+" ");
     }
   }
 }
