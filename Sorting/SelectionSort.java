cf19081c15fdc55462c2bc56623ae4e62d75de7d
rupesh-dev30 <cgcrupesh@gmail.com>
2024-05-03 00:49:43 +0530

    Selection Sort

---
+++ Sorting/SelectionSort.java
@@ -0,0 +1,23 @@
+public class SelectionSort {
+  public static void selectionSort(int[] arr){
+    for(int i=0 ; i<arr.length-1 ; i++){
+      int minPosition = i;
+      for(int j=i+1; j<arr.length; j++){
+        if(arr[minPosition] > arr[j]){
+          minPosition = j;
+        }
+      }
+      int temp = arr[minPosition];
+      arr[minPosition] = arr[i];
+      arr[i] = temp;
+    }
+  }
+  public static void main(String[] args) {
+    int[] arr = {23,56,11,67,45,10,5,32,89};
+    selectionSort(arr);
+    System.out.print("Sorted array : ");
+    for(int i=0 ; i<arr.length; i++){
+      System.out.print(arr[i]+" ");
+    }
+  }
+}
