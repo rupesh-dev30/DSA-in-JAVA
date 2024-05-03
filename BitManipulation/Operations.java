44ff0c90b6a052503303148a087f43653fe85de8
rupesh-dev30 <cgcrupesh@gmail.com>
2024-05-03 17:45:58 +0530

    check ith position set or unset bit

---
+++ BitManipulation/Operations.java
@@ -0,0 +1,26 @@
+/*
+  1) Get ith bit
+                 1 0 0 0 1 0 0 1 1 0 0
+     position : 10 9 8 7 6 5 4 3 2 1 0
+
+     i = 2 then ith bit = 1
+     i = 7 then ith bit = 0
+
+     get ith bit :
+     mask = 1 << i
+     mask & n
+*/
+
+public class Operations {
+  public static void main(String[] args) {
+    int n = 1100;
+    int i = 2;
+    int mask = 1 << i;
+    
+    if((n & mask) == 0){
+      System.out.println("Unset Bit");
+    }else{
+      System.out.println("Set Bit");
+    }
+  }
+}
