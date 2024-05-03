89531448ff2777d49b1bcf59a68cdecf2b05da5a
rupesh-dev30 <cgcrupesh@gmail.com>
2024-05-03 17:06:56 +0530

    Binary Or

---
+++ BitManipulation/BinaryOr.java
@@ -0,0 +1,17 @@
+/*
+   0 | 0 -> 0
+   0 | 1 -> 1
+   1 | 0 -> 1
+   1 | 1 -> 1
+
+   Example: 
+   5:     1 0 1
+   6:     1 1 0
+  5 | 6:  1 1 1   -> 7
+*/
+
+public class BinaryOr {
+  public static void main(String[] args) {
+    System.out.println(5 | 6);
+  }
+}
