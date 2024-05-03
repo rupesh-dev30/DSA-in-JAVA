5473837fc4257d3106512897181eabc2f4f83e81
rupesh-dev30 <cgcrupesh@gmail.com>
2024-05-03 15:56:19 +0530

    CompareTo Function | Largest String in a set of strings

---
+++ String/QuestionLargestSetOfString.java
@@ -0,0 +1,16 @@
+//For a given set of Strings, print the largest string.
+//Lexicographic Order
+
+public class QuestionLargestSetOfString {
+  public static void main(String[] args) {
+    String[] fruits = {"apple", "mango", "banana"};
+
+    String largest = fruits[0];
+    for(int i=1; i<fruits.length; i++){
+      if(largest.compareTo(fruits[i]) < 0){
+        largest = fruits[i];
+      }
+    }
+    System.out.println(largest);
+  }
+}
