680c57b7015a9372ed72754348fe8d89a22fbb7f
rupesh-dev30 <cgcrupesh@gmail.com>
2024-05-04 18:23:58 +0530

    Hybrid Inheritance

---
+++ OOPS/HybridInheritance.java
@@ -0,0 +1,49 @@
+public class HybridInheritance {
+  public static void main(String[] args) {
+    Fish shark = new Fish();
+    //shark.fly();
+    shark.eat();
+    shark.swim();
+  }
+}
+
+//Parent Class
+class Animal{
+  String color;
+
+  void eat(){
+    System.out.println("Eats");
+  }
+
+  void breathe(){
+    System.out.println("Breathes");
+  }
+}
+
+class Mammals extends Animal{
+  int legs;
+  void walk(){
+    System.out.println("Walks");
+  }
+}
+
+class Bird extends Animal{
+  String wings;
+
+  void fly(){
+    System.out.println("Fly");
+  }
+}
+
+class Fish extends Animal{
+  String fins;
+
+  void swim(){
+    System.out.println("Swim");
+  }
+}
+
+class Shark extends Fish{
+  System.out.println("Largest Fish");
+}
+
