126a3ba3f6988a1628fa6f5f0bfe0908835ac22c
rupesh-dev30 <cgcrupesh@gmail.com>
2024-05-04 17:38:30 +0530

    Copy Constructor

--- OOPS/Constructor.java
+++ OOPS/Constructor.java
@@ -16,16 +16,30 @@
 
 public class Constructor {
   public static void main(String[] args) {
+
+    //Constructor Overloading -> Polymorphism
+
+    //Default Constructor
     Student s1 = new Student();
+
+    //Parametrized Contructor
     Student s2 = new Student("Rupesh", 2236977);
     System.out.println(s2.name);
     System.out.println(s2.roll);
+
+    //Copy Constructor
+    Student s3 = new Student(s2);
+    s3.password = "xyz";
+    System.out.println(s3.name);
+    System.out.println(s3.roll);
+    System.out.println(s3.password);
   }
 }
 
 class Student{
   String name;
   int roll;
+  String password;
 
   //Default Constructor
   Student(){
@@ -37,5 +51,12 @@ class Student{
     System.out.println("Parameterized Constructor Called!");
     this.name = name;
     this.roll = roll;
+  }
+
+  //Copy Constructor
+  Student(Student s){
+    System.out.println("Copy Constructor Called!");
+    this.name = s.name;
+    this.roll = s.roll;
   }
 }
