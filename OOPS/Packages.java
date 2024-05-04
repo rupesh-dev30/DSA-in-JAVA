c159fdaa196b80295c008e5560762e5cb746e5ed
rupesh-dev30 <cgcrupesh@gmail.com>
2024-05-04 18:38:04 +0530

    Packages

---
+++ OOPS/Packages.java
@@ -0,0 +1,15 @@
+/*
+    Packages:
+     - Package is a group of similar types of classes, interfaces and sub-packages
+
+     - Inbuilt Packages and User Defined Packages
+*/
+
+import java.util.Scanner;
+//Import the util package which is store in java
+
+public class Packages {
+  //Inbuild Packages
+  Scanner sc = new Scanner(System.in);
+  //System.in is parameter which is passed to Scanner
+}
