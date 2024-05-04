d9a5a41a4415f47e781ccffd4f7bd49ea966880f
rupesh-dev30 <cgcrupesh@gmail.com>
2024-05-04 18:43:24 +0530

    Abstraction

---
+++ OOPS/Abstraction.java
@@ -0,0 +1,21 @@
+/*
+    Abstraction:
+      - Hiding all the unnecessary details and showing only the important parts to the users
+
+      It can be implemented by two method
+        1) Abstract Class     2) Interfaces
+        
+
+      * Difference in Abstraction and Encapsulation
+
+      - Abstraction: 
+        It is a feature of OOPs. It is used to hide the unnecessary information or data from the user but shows the essential data that is useful for the user. It can be achieved by using the interface and the abstract class. In interfaces, only the methods are exposed to the end-user. The best example of abstraction is a TV remote. The user only interacts with the outer interface that is nothing but keys. The user only knows which key to press for what function.
+
+      - Encapsulation:
+        It is also a feature of OOP. It is used to bind up the data into a single unit called class. It provides the mechanism which is known as data hiding. It is an important feature of OOPs. It prevents to access data members from the outside of the class. It is also necessary from the security point of view.
+
+*/
+
+public class Abstraction {
+  
+}
