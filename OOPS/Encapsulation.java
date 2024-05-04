7d6feb26b235e8e18ea47c24c93bb7b38889a33a
rupesh-dev30 <cgcrupesh@gmail.com>
2024-05-04 17:21:33 +0530

    Encapsulation

---
+++ OOPS/Encapsulation.java
@@ -0,0 +1,7 @@
+//Encapsulation is defined as the wrapping up of data & methods under a single unit. It also implements data hiding.
+
+public class Encapsulation {
+  public static void main(String[] args) {
+    
+  }
+}
