bad870eb86f9c7cf37cff7deaffc1826e8bd4036
rupesh-dev30 <cgcrupesh@gmail.com>
2024-05-04 18:08:50 +0530

    Destructor

---
+++ OOPS/Destructor.java
@@ -0,0 +1,16 @@
+/*
+    Destructor:
+
+    - It is a special method that automatically gets called when an object is no longer used. When an object completes its life-cycle the garbage collector deletes that object and deallocates or releases the memory occupied by the object.
+
+    - Destructors are used to release the resources that are allocated to an object.
+
+    - Destructors are also known as the finalizer method.
+
+    - No need to create destructor in java.
+    
+*/
+
+public class Destructor {
+  
+}
