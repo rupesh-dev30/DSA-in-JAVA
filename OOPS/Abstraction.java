65389f27ded52004561eb8f956530771ef874439
rupesh-dev30 <cgcrupesh@gmail.com>
2024-05-04 19:00:53 +0530

    Abstraction , Abstract Class , Method, Constructor

--- OOPS/Abstraction.java
+++ OOPS/Abstraction.java
@@ -4,7 +4,7 @@
 
       It can be implemented by two method
         1) Abstract Class     2) Interfaces
-        
+
 
       * Difference in Abstraction and Encapsulation
 
@@ -14,8 +14,66 @@
       - Encapsulation:
         It is also a feature of OOP. It is used to bind up the data into a single unit called class. It provides the mechanism which is known as data hiding. It is an important feature of OOPs. It prevents to access data members from the outside of the class. It is also necessary from the security point of view.
 
+
+
+        Abstract Class: 
+         - Cannot create an instance of abstract class
+         - Can have abstract/non-abstract methods
+         - Can have Constructors(NOTE)
+
 */
 
 public class Abstraction {
-  
+  public static void main(String[] args) {
+    Horse h = new Horse();
+    h.eat();
+    h.walk();
+    System.out.println(h.color);
+
+    Chicken c = new Chicken();
+    c.eat();
+    c.walk();
+
+    //Not possible
+    // Animal a = new Animal() {};
+    
+  }
+}
+
+//Abstract Class means -> no object creation
+abstract class Animal{   
+  String color;
+
+  //Constructor
+  Animal(){
+    color = "brown";
+  }   
+
+  //Non Abstract Method
+  void eat(){
+    System.out.println("Animal Eat!");
+  }
+
+  //Abstract Method -> It means no implementation
+  abstract void walk();
+}
+
+class Horse extends Animal{
+  void changeColor(){
+    color = "dark brown";
+  }
+
+  void walk(){
+    System.out.println("Walks on 4 legs!");
+  }
+}
+
+class Chicken extends Animal{
+  void changeColor(){
+    color = "yellow";
+  }
+
+  void walk(){
+    System.out.println("Walks on 2 legs!");
+  }
 }
