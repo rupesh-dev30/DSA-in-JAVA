5bb356f7ed51627bfb11e73daa0e87faa29efcf4
rupesh-dev30 <cgcrupesh@gmail.com>
2024-05-04 17:30:29 +0530

    Types of Contructor (Deafult and Parametrized)

--- OOPS/Constructor.java
+++ OOPS/Constructor.java
@@ -6,13 +6,20 @@
     - Constructors are only called once, at object creation.
     - Memory allocation happends when constructor is called
 
+
+    Types of Constructor: 
+    1. Default Constructor / Non Parameterized Constructor
+    2. Parameterized Constructor
+    3. Copy Constructor
+  
 */
 
 public class Constructor {
   public static void main(String[] args) {
-    Student s1 = new Student("Rupesh", 2236977);
-    System.out.println(s1.name);
-    System.out.println(s1.roll);
+    Student s1 = new Student();
+    Student s2 = new Student("Rupesh", 2236977);
+    System.out.println(s2.name);
+    System.out.println(s2.roll);
   }
 }
 
@@ -20,8 +27,14 @@ class Student{
   String name;
   int roll;
 
+  //Default Constructor
+  Student(){
+    System.out.println("Default Constructor Called!");
+  }
+
+  //Parameterized Constructor
   Student(String name , int roll){
-    System.out.println("Constructor Called!");
+    System.out.println("Parameterized Constructor Called!");
     this.name = name;
     this.roll = roll;
   }
