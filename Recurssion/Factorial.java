cffadb30d68fddf956039d79f19a523eaa7fe321
rupesh-dev30 <cgcrupesh@gmail.com>
2024-05-05 20:39:00 +0530

    Factorial

---
+++ Recurssion/Factorial.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Factorial {
+  public static int factorial(int num){
+    if(num == 1){
+      return 1;
+    }
+
+    int fact = num * factorial(num - 1);
+    return fact;
+  }
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int num = sc.nextInt();
+    sc.close();
+
+    int result = factorial(num);
+    System.out.println(result);
+  }
+}
