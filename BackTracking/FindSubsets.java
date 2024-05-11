f4758b6ffd42cbbb9380edbe5a230bb0fb16517d
rupesh-dev30 <cgcrupesh@gmail.com>
2024-05-11 18:51:05 +0530

    Subset uisng stringBuilder

--- BackTracking/FindSubsets.java
+++ BackTracking/FindSubsets.java
@@ -1,5 +1,5 @@
 public class FindSubsets {
-  public static void findSubset(String str, int index, String ans){
+  public static void findSubset(String str, int index, StringBuilder ans){
     //Base Case 
     if(index == str.length()){
       if(ans.length() == 0) 
@@ -13,13 +13,18 @@ public class FindSubsets {
 
     //Recurssion Relation
     //YES CHOICE (if character want to come)
-    findSubset(str, index+1, ans+str.charAt(index));
+    findSubset(str, index+1, ans.append(str.charAt(index)));
+
+    //Backtracking
+    ans.deleteCharAt(ans.length()-1);
 
     //NO CHOICE (if character not want to come)
     findSubset(str, index+1, ans);
+
+    
   }
   public static void main(String[] args) {
     String str = "abc";
-    findSubset(str, 0, "");
+    findSubset(str, 0, new StringBuilder(""));
   }
 }
