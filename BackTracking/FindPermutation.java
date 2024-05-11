b73302352c500bb40209ee2022378f0d8280f790
rupesh-dev30 <cgcrupesh@gmail.com>
2024-05-11 19:10:23 +0530

    permutation

---
+++ BackTracking/FindPermutation.java
@@ -0,0 +1,22 @@
+public class FindPermutation {
+  public static void findPermutation(String str, String ans){
+    if(str.length() == 0){
+      System.out.println(ans);
+      return;
+    }
+
+    //Recurssion Relation
+    for(int i=0 ; i<str.length() ; i++){
+      char curr = str.charAt(i);
+
+      // "abcde" => "ab" + "de" = "abde"
+      String NewStr = str.substring(0, i) + str.substring(i+1);
+
+      findPermutation(NewStr, ans+curr);
+    }
+  }
+  public static void main(String[] args) {
+    String str = "abc";
+    findPermutation(str, "");
+  }
+}
