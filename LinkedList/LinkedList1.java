f756b2737c9cc2c5a7f99b2a4657cb75bf5a954c
rupesh-dev30 <cgcrupesh@gmail.com>
2024-05-19 12:05:19 +0530

    printing node of linked list

--- LinkedList/LinkedList1.java
+++ LinkedList/LinkedList1.java
@@ -37,15 +37,34 @@ public class LinkedList1 {
     }
 
     tail.next = newNode;
-    newNode.next = null;
     tail = newNode;
+  }
+
+  public void print(){
+    Node temp = head;
+    if(head == null){
+      System.out.println("Linked List is Empty!!");
+      return;
+    }
+
+    while(temp!=null){
+      System.out.print(temp.data+"->");
+      temp = temp.next;
+    }
+    System.out.println("null");
   }
 
   public static void main(String[] args) {
     LinkedList1 l1 = new LinkedList1();
+    l1.print();
     l1.addFirst(2);
+    l1.print();
     l1.addFirst(1);
+    l1.print();
     l1.addLast(3);
+    l1.print();
     l1.addLast(4);
+    l1.print();
+    
   }
 }
