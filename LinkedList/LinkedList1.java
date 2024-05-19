e1a54eea9e40055a17f1c7f88699888afb062dba
rupesh-dev30 <cgcrupesh@gmail.com>
2024-05-19 12:14:16 +0530

    Add a node at any position of Linked List

--- LinkedList/LinkedList1.java
+++ LinkedList/LinkedList1.java
@@ -40,6 +40,24 @@ public class LinkedList1 {
     tail = newNode;
   }
 
+  public void addMiddle(int index, int data){
+    if(index == 0){
+      addFirst(data);
+    }
+    
+    Node newNode = new Node(data);
+    Node temp = head;
+    int i = 0;
+
+    while(i<index-1){
+      temp = temp.next;
+      i++;
+    }
+
+    newNode.next = temp.next;
+    temp.next = newNode;
+  }
+
   public void print(){
     Node temp = head;
     if(head == null){
@@ -65,6 +83,9 @@ public class LinkedList1 {
     l1.print();
     l1.addLast(4);
     l1.print();
+    l1.addMiddle(3, 5);
+    l1.print();
+
     
   }
 }
