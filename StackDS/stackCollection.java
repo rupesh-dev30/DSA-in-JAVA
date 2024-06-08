f43f3730ac225814bb470b17554595e137cbbefb
rupesh-dev30 <cgcrupesh@gmail.com>
2024-06-09 02:38:51 +0530

    stack using collection

---
+++ StackDS/stackCollection.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class stackCollection {
+  public static void main(String[] args) {
+    Stack<Integer> s = new Stack<>();
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    s.push(4);
+    s.push(5);
+
+    while(!s.isEmpty()){
+      System.out.print(s.peek() + " ");
+      s.pop();
+    }
+  }
+}
