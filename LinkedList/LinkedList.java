93f688b9207dcbe1b6550559b0e70e25870fe6c2
rupesh-dev30 <cgcrupesh@gmail.com>
2024-06-09 02:08:35 +0530

    stackIntro

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -1,17 +1,18 @@
 public class LinkedList {
-  public static class Node{
+  public static class Node {
     int data;
     Node next;
 
-    public Node(int data){
+    public Node(int data) {
       this.data = data;
       this.next = null;
     }
   }
+
   public static Node head;
   public static Node tail;
 
-  //Methods
+  // Methods
 
   public static void main(String[] args) {
     LinkedList li = new LinkedList();
