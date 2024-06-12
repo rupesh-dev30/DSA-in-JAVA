1b55f05690d21b4a61732bce3f1703f943957268
rupesh-dev30 <cgcrupesh@gmail.com>
2024-06-13 00:35:24 +0530

    duplicate parenthesis

---
+++ StackDS/duplicateParenthesis.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class duplicateParenthesis {
+  public static boolean isDuplicate(String str){
+    Stack<Character> s = new Stack<>();
+
+    for(int i=0; i<str.length(); i++){
+      char ch = str.charAt(i);
+      
+      //Closing
+      if(ch == ')'){
+        int count = 0;
+        while(s.peek() != '('){
+          s.pop();
+          count++;
+        }
+        if(count<1){
+          return true;
+        } else {
+          s.pop();
+        }
+      }
+
+      //Opening
+      else {
+        s.push(ch);
+      }
+    }
+    return false;
+  }
+  public static void main(String[] args) {
+    String str = "((a+b))";
+    String str2 = "(a+b)";
+    System.out.println(isDuplicate(str2));
+  }
+}
