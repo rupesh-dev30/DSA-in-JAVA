c40abd0cd80936e98eb84651a0e94b9dc8fd57e7
rupesh-dev30 <cgcrupesh@gmail.com>
2024-06-18 14:42:17 +0530

    Queue Collection using ArrayDeque and LinkedList

--- QueueDS/QueueCollection.java
+++ QueueDS/QueueCollection.java
@@ -4,6 +4,7 @@ public class QueueCollection {
   public static void main(String[] args) {
     // Queue q = new Queue(5);
     Queue<Integer> q = new LinkedList<>();
+    Queue<Integer> dq = new ArrayDeque<>();
     //We cant create a object of queue because queue is interface. LinkedList and ArrayDeque are the two class which implement queue in java.
       
     q.add(1);
@@ -15,6 +16,17 @@ public class QueueCollection {
     while(!q.isEmpty()){
       System.out.println(q.peek());
       q.remove();
+    }
+
+    dq.add(1);
+    dq.add(2);
+    dq.add(3);
+    dq.add(4);
+    dq.add(5);
+
+    while(!dq.isEmpty()){
+      System.out.println(dq.peek());
+      dq.remove();
     }
   }
 }
