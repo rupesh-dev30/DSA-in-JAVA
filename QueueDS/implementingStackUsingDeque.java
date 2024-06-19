6b67a4517574c6bd224d92feb51702276dcc06af
rupesh-dev30 <cgcrupesh@gmail.com>
2024-06-19 14:35:12 +0530

    Implementing Stack using Deque

---
+++ QueueDS/implementingStackUsingDeque.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.util.LinkedList;
+
+public class implementingStackUsingDeque {
+  static class Stack{
+    Deque<Integer> deque = new LinkedList<>();
+
+    public void push(int data){
+      deque.addLast(data);
+    }
+
+    public int pop(){
+      return deque.removeLast();
+    }
+
+    public int peek(){
+      return deque.getLast();
+    }
+  }
+  public static void main(String[] args) {
+    Stack s = new Stack();
+    s.push(1);
+    s.push(2);
+    s.push(3);
+
+    System.out.println(s.peek() + " ");
+  }
+}
