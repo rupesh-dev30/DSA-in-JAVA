2442c1b29ad24bd8add4f5131b79d744b9d33cc2
rupesh-dev30 <cgcrupesh@gmail.com>
2024-07-09 12:20:49 +0530

    height of the tree

---
+++ Trees/BinaryTree/diameterOfATree.java
@@ -0,0 +1,51 @@
+public class diameterOfATree {
+  static class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data) {
+      this.data = data;
+      this.left = null;
+      this.right = null;
+    }
+  }
+
+  // Heigth of the tree
+  public static int height(Node root) {
+    if (root == null) {
+      return 0;
+    }
+
+    int leftHeight = height(root.left);
+    int rightHeight = height(root.right);
+    return Math.max(leftHeight, rightHeight) + 1;
+  }
+
+  public static int diameter(Node root) {
+    if (root == null) {
+      return 0;
+    }
+
+    int leftDiameter = diameter(root.left);
+    int leftHeight = height(root.left);
+    int rightDiameter = diameter(root.right);
+    int rightHeight = height(root.right);
+
+    int selfDiameter = leftHeight + rightHeight + 1;
+
+    return Math.max(selfDiameter, Math.max(leftDiameter, rightDiameter));
+  }
+
+  public static void main(String[] args) {
+    Node root = new Node(1);
+    root.left = new Node(2);
+    root.right = new Node(3);
+    root.left.left = new Node(4);
+    root.left.right = new Node(5);
+    root.right.left = new Node(6);
+    root.right.right = new Node(7);
+
+    System.out.println(diameter(root));
+  }
+}
