25b764a72633251c61dbda3d9a3be287ee6a6e11
rupesh-dev30 <cgcrupesh@gmail.com>
2024-07-09 12:33:26 +0530

    improve complexities of diameter of the tree

--- Trees/BinaryTree/diameterOfATree.java
+++ Trees/BinaryTree/diameterOfATree.java
@@ -22,19 +22,42 @@ public class diameterOfATree {
     return Math.max(leftHeight, rightHeight) + 1;
   }
 
-  public static int diameter(Node root) {
-    if (root == null) {
-      return 0;
+  // public static int diameter(Node root) {
+  //   if (root == null) {
+  //     return 0;
+  //   }
+
+  //   int leftDiameter = diameter(root.left);
+  //   int leftHeight = height(root.left);
+  //   int rightDiameter = diameter(root.right);
+  //   int rightHeight = height(root.right);
+
+  //   int selfDiameter = leftHeight + rightHeight + 1;
+
+  //   return Math.max(selfDiameter, Math.max(leftDiameter, rightDiameter));
+  // }
+
+  static class Info {
+    int diameter;
+    int height;
+
+    public Info(int diameter, int height){
+      this.diameter = diameter;
+      this.height = height;
     }
+  }
 
-    int leftDiameter = diameter(root.left);
-    int leftHeight = height(root.left);
-    int rightDiameter = diameter(root.right);
-    int rightHeight = height(root.right);
+  public static Info diameter(Node root){
+    if(root == null){
+      return new Info(0, 0);
+    }
+    Info leftInfo = diameter(root.left);
+    Info rightInfo = diameter(root.right);
 
-    int selfDiameter = leftHeight + rightHeight + 1;
+    int diameter = Math.max(Math.max(leftInfo.diameter, rightInfo.diameter), leftInfo.height + rightInfo.height + 1);
 
-    return Math.max(selfDiameter, Math.max(leftDiameter, rightDiameter));
+    int height = Math.max(leftInfo.height, rightInfo.height) + 1;
+    return new Info(diameter, height);
   }
 
   public static void main(String[] args) {
@@ -46,6 +69,6 @@ public class diameterOfATree {
     root.right.left = new Node(6);
     root.right.right = new Node(7);
 
-    System.out.println(diameter(root));
+    System.out.println(diameter(root).diameter);
   }
 }
