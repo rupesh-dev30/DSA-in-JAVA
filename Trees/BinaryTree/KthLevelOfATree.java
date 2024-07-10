3829dba9d6abaecdb408daea672668981a18248a
rupesh-dev30 <cgcrupesh@gmail.com>
2024-07-10 11:53:31 +0530

    kth level level wise method

---
+++ Trees/BinaryTree/KthLevelOfATree.java
@@ -0,0 +1,59 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class KthLevelOfATree {
+  static class Node {
+    int data;
+    Node left, right;
+
+    Node(int data) {
+      this.data = data;
+      this.left = null;
+      this.right = null;
+    }
+  }
+
+  public static void kthlLevel(Node root, int k) {
+    if (root == null)
+      return;
+
+    Queue<Node> q = new LinkedList<>();
+    q.add(root);
+    q.add(null);
+    int level = 1;
+
+    while (!q.isEmpty()) {
+      Node curr = q.remove();
+      if (curr == null) {
+        if (q.isEmpty())
+          break;
+        else {
+          q.add(null);
+          level++;
+        }
+      } else {
+        if (level == k) {
+          System.out.print(curr.data + " ");
+        }
+        if (curr.left != null) {
+          q.add(curr.left);
+        }
+        if (curr.right != null) {
+          q.add(curr.right);
+        }
+      }
+    }
+  }
+
+  public static void main(String[] args) {
+    Node root = new Node(1);
+    root.left = new Node(2);
+    root.right = new Node(3);
+    root.left.left = new Node(4);
+    root.left.right = new Node(5);
+    root.right.left = new Node(6);
+    root.right.right = new Node(7);
+
+    kthlLevel(root, 3);
+  }
+}
