5d4a6d7c6129552d50633f4dd92f7904733e3fcd
rupesh-dev30 <cgcrupesh@gmail.com>
2024-07-10 11:57:53 +0530

    recusrsivel kth level node

--- Trees/BinaryTree/KthLevelOfATree.java
+++ Trees/BinaryTree/KthLevelOfATree.java
@@ -13,36 +13,46 @@ public class KthLevelOfATree {
     }
   }
 
-  public static void kthlLevel(Node root, int k) {
-    if (root == null)
+  // public static void kthlLevel(Node root, int k) {
+  //   if (root == null)
+  //     return;
+
+  //   Queue<Node> q = new LinkedList<>();
+  //   q.add(root);
+  //   q.add(null);
+  //   int level = 1;
+
+  //   while (!q.isEmpty()) {
+  //     Node curr = q.remove();
+  //     if (curr == null) {
+  //       if (q.isEmpty())
+  //         break;
+  //       else {
+  //         q.add(null);
+  //         level++;
+  //       }
+  //     } else {
+  //       if (level == k) {
+  //         System.out.print(curr.data + " ");
+  //       }
+  //       if (curr.left != null) {
+  //         q.add(curr.left);
+  //       }
+  //       if (curr.right != null) {
+  //         q.add(curr.right);
+  //       }
+  //     }
+  //   }
+  // }
+
+  public static void kthLevelRec(Node root, int level, int k){
+    if(level == k){
+      System.out.print(root.data + " ");
       return;
-
-    Queue<Node> q = new LinkedList<>();
-    q.add(root);
-    q.add(null);
-    int level = 1;
-
-    while (!q.isEmpty()) {
-      Node curr = q.remove();
-      if (curr == null) {
-        if (q.isEmpty())
-          break;
-        else {
-          q.add(null);
-          level++;
-        }
-      } else {
-        if (level == k) {
-          System.out.print(curr.data + " ");
-        }
-        if (curr.left != null) {
-          q.add(curr.left);
-        }
-        if (curr.right != null) {
-          q.add(curr.right);
-        }
-      }
     }
+
+    kthLevelRec(root.left, level+1, k);
+    kthLevelRec(root.right, level+1, k);
   }
 
   public static void main(String[] args) {
@@ -54,6 +64,7 @@ public class KthLevelOfATree {
     root.right.left = new Node(6);
     root.right.right = new Node(7);
 
-    kthlLevel(root, 3);
+    // kthlLevel(root,3);
+    kthLevelRec(root,1,3);
   }
 }
