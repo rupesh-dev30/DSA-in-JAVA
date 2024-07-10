cb75b47ef4d76e6e7ab68b11f6f40a2fe2079198
rupesh-dev30 <cgcrupesh@gmail.com>
2024-07-10 14:13:30 +0530

    imporve LCA

--- Trees/BinaryTree/LowestCommonAncestor.java
+++ Trees/BinaryTree/LowestCommonAncestor.java
@@ -5,50 +5,70 @@ public class LowestCommonAncestor {
     int data;
     Node left, right;
 
-    Node(int data){
+    Node(int data) {
       this.data = data;
       this.left = null;
       this.right = null;
     }
   }
 
-  public static boolean getPath(Node root, int n, ArrayList<Node> path){
-    if(root == null){
+  public static boolean getPath(Node root, int n, ArrayList<Node> path) {
+    if (root == null) {
       return false;
     }
 
     path.add(root);
-    if(root.data == n){
+    if (root.data == n) {
       return true;
     }
 
     boolean foundLeft = getPath(root.left, n, path);
     boolean foundRight = getPath(root.right, n, path);
 
-    if(foundLeft || foundRight){
+    if (foundLeft || foundRight) {
       return true;
     }
 
     path.remove(path.size() - 1);
     return false;
-  } 
+  }
 
-  public static Node lca(Node root, int n1, int n2){
+  public static Node lca(Node root, int n1, int n2) {
     ArrayList<Node> path1 = new ArrayList<>();
     ArrayList<Node> path2 = new ArrayList<>();
 
     getPath(root, n1, path1);
     getPath(root, n2, path2);
 
-    int i=0;
-    for(; i<path1.size() && i<path2.size(); i++){
-      if(path1.get(i) != path2.get(i)){
+    int i = 0;
+    for (; i < path1.size() && i < path2.size(); i++) {
+      if (path1.get(i) != path2.get(i)) {
         break;
       }
     }
-    Node lca = path1.get(i-1);
+    Node lca = path1.get(i - 1);
     return lca;
   }
+
+  public static Node lca2(Node root, int n1, int n2) {
+    if (root == null || root.data == n1 || root.data == n2) {
+      return root;
+    }
+
+    Node leftLca = lca2(root.left, n1, n2);
+    Node rightLca = lca2(root.right, n1, n2);
+
+    // leftLca = val but rightLca = null
+    if (rightLca == null) {
+      return leftLca;
+    }
+    if (leftLca == null) {
+      return rightLca;
+    }
+
+    return root;
+  }
+
   public static void main(String[] args) {
     Node root = new Node(1);
     root.left = new Node(2);
@@ -58,7 +78,7 @@ public class LowestCommonAncestor {
     root.right.left = new Node(6);
     root.right.right = new Node(7);
 
-    int n1 = 4, n2 = 7;
-    System.out.println(lca(root, n1, n2).data);
+    int n1 = 4, n2 = 5;
+    System.out.println(lca2(root, n1, n2).data);
   }
 }
