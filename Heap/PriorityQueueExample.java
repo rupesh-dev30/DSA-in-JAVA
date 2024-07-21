7fbe054771992779ff45d76fb30b3bdc6eb4c506
rupesh-dev30 <cgcrupesh@gmail.com>
2024-07-21 15:28:45 +0530

    priority queue

---
+++ Heap/PriorityQueueExample.java
@@ -0,0 +1,15 @@
+import java.util.*;
+  public static void main(String[] args) {
+    PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+    pq.add(3);
+    pq.add(4);
+    pq.add(1);
+    pq.add(7);
+
+    while(!pq.isEmpty()){
+      System.out.println(pq.peek());
+      pq.remove();
+    }
+  }
+}
