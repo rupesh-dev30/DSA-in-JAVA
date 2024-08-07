77a7a5b37b94874bc25943ba93df436984d5e9c3
rupesh-dev30 <cgcrupesh@gmail.com>
2024-08-07 14:25:31 +0530

    heap introduction

---
+++ Heap/Heap.java
@@ -0,0 +1,17 @@
+// A Heap is a complete binary tree data structure that satisfies the heap property: for every node, the value of its children is greater than or equal to its own value. Heaps are usually used to implement priority queues, where the smallest (or largest) element is always at the root of the tree.
+
+/*
+    Binary Tree:
+      at most 2 children
+
+    Complete Binary Tree: 
+      CBT is a BT in which all the levels are completley filled except possibly the last one, which is filled from left to right
+
+    Heap Order Property: 
+      Children >= Parent(maxHeap)
+      Children <= Parent(minHeap)
+*/
+
+public class Heap {
+  
+}
